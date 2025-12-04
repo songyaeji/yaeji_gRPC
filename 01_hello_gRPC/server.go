85da6e40eba4116518f2c6c359036b444eb20af8
songyaeji <skaehdlf0318@gmail.com>
2025-12-04 22:01:19 +0900

    Add struct UnimplementedMyServiceServer as server and func MyFunction

---
+++ 01_hello_gRPC/server.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	context "context"
+)
+
+// (4) protoc가 생성한 Servicer 클래스를 base class로 해서 원격 호출될 함수들을 멤버로 갖는 서버 구조체를 생성함
+type server struct {
+	UnimplementedMyServiceServer
+}
+
+// (5) 서버 구조체에 원격 호출될 함수에 대한 rpc 함수를 작성함
+// (5.1) proto 파일내 정의한 rpc 함수 이름에 대응하는 메서드를 작성함
+func (s *server) MyFunction(ctx context.Context, request *MyNumber) (*MyNumber, error) {
+	// (5.2) proto 파일내 message 이름과 동일한 message 구조체를 생성하여 응답 전달 용도로 사용함
+	response := &MyNumber{}
+	// (5.3) proto 파일내 message 이름과 동일한 message 구조체의 변수에 원격 함수의 수행 결과를 저장함
+	// 앞서 (3)의 원격 호출할 함수에게 client로 부터 받은 입력 파라메타를 전달하고 결과를 가져옴
+	response.Value = int32(my_func(int(request.Value)))
+	// (5.4) 원격 함수 호출 결과를 client에게 돌려줌
+	return response, nil
+}
