dfec7c59ca709cfb3568d4052e86b605c429717a
songyaeji <skaehdlf0318@gmail.com>
2025-12-04 22:02:27 +0900

    Modify import section and Add main func

--- 01_hello_gRPC/server.go
+++ 01_hello_gRPC/server.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	context "context"
+	fmt "fmt"
+	net "net"
+
+	grpc "google.golang.org/grpc"
 )
 
 // (4) protoc가 생성한 Servicer 클래스를 base class로 해서 원격 호출될 함수들을 멤버로 갖는 서버 구조체를 생성함
@@ -19,4 +23,21 @@ func (s *server) MyFunction(ctx context.Context, request *MyNumber) (*MyNumber, 
 	response.Value = int32(my_func(int(request.Value)))
 	// (5.4) 원격 함수 호출 결과를 client에게 돌려줌
 	return response, nil
+}
+
+func main() {
+	// (6) grpc.server를 생성함
+	// Go에서는 grpc.NewServer()를 사용하여 서버를 생성함
+	grpcServer := grpc.NewServer()
+
+	// (7) RegisterMyServiceServer()를 사용해서, grpc.server에 (4)의 Servicer를 추가함
+	RegisterMyServiceServer(grpcServer, &server{})
+
+	// (8) grpc.server의 통신 포트를 열고, start()로 서버를 실행함
+	lis, _ := net.Listen("tcp", ":50051")
+	fmt.Println("Starting server. Listening on port 50051.")
+	grpcServer.Serve(lis)
+
+	// (9) grpc.server가 유지되도록 프로그램 실행을 유지함
+	// server.Serve()가 블로킹되어 자동으로 유지됨
 }
