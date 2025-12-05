aa1553252cbd94b1e7990a58f4653b5419ccf63b
songyaeji <skaehdlf0318@gmail.com>
2025-12-06 00:28:00 +0900

    Create server.go file code except func MyFunction

--- 02_bidirection_streaming/server.go
+++ 02_bidirection_streaming/server.go
@@ -1,4 +1,33 @@
 //go:build !client
 // +build !client
 
+// Bidirectional streaming gRPC server
+
 package main
+
+import (
+	fmt "fmt"
+	net "net"
+
+	grpc "google.golang.org/grpc"
+)
+
+type BidirectionalService struct {
+	UnimplementedBidirectionalServer
+}
+
+/*
+func MyFunction
+*/
+
+func serve() {
+	server := grpc.NewServer()
+	RegisterBidirectionalServer(server, &BidirectionalService{})
+	fmt.Println("Starting server. Listening on port 50051.")
+	lis, _ := net.Listen("tcp", ":50051")
+	server.Serve(lis)
+}
+
+func main() {
+	serve()
+}
