6017e960354f0fe5f91dd3c3c3cfe7c569574c25
songyaeji <skaehdlf0318@gmail.com>
2025-12-06 23:39:19 +0900

    Fix server.go Func

--- 04_serverstreaming/server.go
+++ 04_serverstreaming/server.go
@@ -5,6 +5,13 @@
 
 package main
 
+import (
+	fmt "fmt"
+	net "net"
+
+	grpc "google.golang.org/grpc"
+)
+
 func make_message(message string) *Message {
 	return &Message{
 		Message: message,
@@ -13,4 +20,33 @@ func make_message(message string) *Message {
 
 type ServerStreamingService struct {
 	UnimplementedServerStreamingServer
+}
+
+func (s *ServerStreamingService) GetServerResponse(request *Number, stream grpc.ServerStreamingServer[Message]) error {
+	messages := []*Message{
+		make_message("message #1"),
+		make_message("message #2"),
+		make_message("message #3"),
+		make_message("message #4"),
+		make_message("message #5"),
+	}
+	fmt.Printf("Server processing gRPC server-streaming {%d}.\n", request.Value)
+	for _, message := range messages {
+		if err := stream.Send(message); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func serve() {
+	server := grpc.NewServer()
+	RegisterServerStreamingServer(server, &ServerStreamingService{})
+	fmt.Println("Starting server. Listening on port 50051.")
+	lis, _ := net.Listen("tcp", ":50051")
+	server.Serve(lis)
+}
+
+func main() {
+	serve()
 }
