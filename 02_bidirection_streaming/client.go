e4248df9473a83b1fb13c80718bf51a818333443
songyaeji <skaehdlf0318@gmail.com>
2025-12-06 15:21:40 +0900

    Create client.go

--- 02_bidirection_streaming/client.go
+++ 02_bidirection_streaming/client.go
@@ -1,4 +1,51 @@
 //go:build client
 // +build client
 
+// Bidirectional streaming gRPC client
+
 package main
+
+import (
+	context "context"
+	fmt "fmt"
+	log "log"
+
+	grpc "google.golang.org/grpc"
+	credentials "google.golang.org/grpc/credentials/insecure"
+)
+
+func make_message(message string) *Message {
+	return &Message{
+		Message: message,
+	}
+}
+
+func generate_messages() []*Message {
+	messages := []*Message{
+		make_message("message #1"),
+		make_message("message #2"),
+		make_message("message #3"),
+		make_message("message #4"),
+		make_message("message #5"),
+	}
+	for _, msg := range messages {
+		fmt.Printf("[client to server] %s\n", msg.Message)
+	}
+	return messages
+}
+
+func run() {
+	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(credentials.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := NewBidirectionalClient(conn)
+	ctx := context.Background()
+	send_message(client, ctx)
+}
+
+func main() {
+	run()
+}
